e4a39da25d3180be3dc2b05c2c69ae38ea285ffa
mcprentiss <mcprentiss@gmail.com>
2026-03-08 20:31:20 +0000

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19,6 +19,27 @@ sudo apt install -y \
     lsb-release \
     python3-pip
 
+# 2.3 Install pyenv developement libraries 
+echo "--- Installing pyenv developement libraries ---"
+sudo apt install -y \ 
+     make \
+     build-essential \
+     libssl-dev \
+     zlib1g-dev \
+     libbz2-dev \
+     libreadline-dev \
+     libsqlite3-dev \
+     curl \
+     git \
+     libncursesw5-dev \ 
+     xz-utils \
+     tk-dev \
+     libxml2-dev \
+     libxmlsec1-dev \ 
+     libffi-dev \
+     liblzma-dev \
+     libzstd-dev
+
 # 3. Install i3 Ecosystem & Utilities
 echo "--- Installing i3wm Desktop Utilities ---"
 sudo apt install -y \
